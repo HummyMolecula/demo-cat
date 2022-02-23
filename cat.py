d6d863b5db1572eb2a6fb6b05eb2637b7828aa70
HummyMolecula <dragonitdeveloper@gmail.com>
2022-02-23 19:19:49 +0200

    cats data output update

--- cat.py
+++ cat.py
@@ -8,11 +8,18 @@ class Cat:
         self.age = age
         self.isHappy = happy
 
+    def get_data(self):
+        print("Name:", self.name)
+        print("Age:", self.age)
+        print("Happy?:", self.isHappy)
+
 cat1 = Cat()
 cat1.set_data("Barsik", 3, True)
 
 cat2 = Cat()
 cat2.set_data("Schopen", 2)
 
-print("First cat:", cat1.name)
-print("Second cat:", cat2.name)
+print("First cat:")
+cat1.get_data()
+print("Second cat:")
+cat2.get_data()
