4128e46d1baa9d6ad4eb715bfb5b04ddd5d9fadc
HummyMolecula <dragonitdeveloper@gmail.com>
2022-02-23 19:11:33 +0200

    first commit

---
+++ cat.py
@@ -0,0 +1,20 @@
+from email.policy import compat32
+
+
+class Cat:
+    name = None
+    age = None
+    isHappy = None
+
+cat1 = Cat()
+cat1.name = "Barsik"
+cat1.age = 3
+cat1.isHappy = True
+
+cat2 = Cat()
+cat2.name = "Shopen"
+cat2.age = 4
+cat2.isHappy = True
+
+print("First cat:", cat1.name)
+print("Second cat:", cat2.name)
